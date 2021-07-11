3196c88d84b854ac41f31b51f6b38c717924a00c
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-07-11 23:00:00 +0300

    Support the cell generator in the Field.__init__() method

--- step_001.py
+++ step_001.py
@@ -1,6 +1,13 @@
 class Field:
-    def __init__(self, width, height):
-        self._cells = [[False] * width] * height
+    def __init__(self, width, height, generator=lambda x, y: False):
+        self._cells = []
+        for y in range(height):
+            line = []
+            for x in range(width):
+                cell = generator(x, y)
+                line.append(cell)
+
+            self._cells.append(line)
 
     def for_each_cell(self, handler):
         for y, line in enumerate(self._cells):
--- step_001_test.py
+++ step_001_test.py
@@ -1,6 +1,7 @@
 import os.path
 import importlib.util
 import unittest
+import random
 
 # workaround for loading the module from the neighbor file
 # without defining the parent module
@@ -10,13 +11,33 @@ step_001 = importlib.util.module_from_spec(step_001_spec)
 step_001_spec.loader.exec_module(step_001)
 
 class TestField(unittest.TestCase):
-    def test_init(self):
+    def test_init_with_default_generator(self):
         field = step_001.Field(2, 3)
 
         self.assertEqual(field._cells, [
             [False, False],
             [False, False],
             [False, False],
+        ])
+
+    def test_init_with_custom_generator(self):
+        field = step_001.Field(2, 3, lambda x, y: (x > 0 and y > 0) or y == 2)
+
+        self.assertEqual(field._cells, [
+            [False, False],
+            [False, True],
+            [True, True],
+        ])
+
+    def test_init_with_random_generator(self):
+        random.seed(1) # reset the random generator for the test reproducibility
+
+        field = step_001.Field(2, 3, lambda x, y: random.choice([False, True]))
+
+        self.assertEqual(field._cells, [
+            [False, False],
+            [True, False],
+            [True, True],
         ])
 
     def test_for_each_cell(self):
