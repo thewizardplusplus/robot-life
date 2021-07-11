fa4061d0f8ffcebcf00954c86d3557485c91f2e9
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-07-11 23:00:00 +0300

    Add the run_field() function

--- robot_life/console.py
+++ robot_life/console.py
@@ -1,3 +1,5 @@
+import time
+
 # https://www.conwaylife.com/wiki/Plaintext
 def to_plaintext(field):
     characters = []
@@ -11,3 +13,17 @@ def to_plaintext(field):
     field.handle_cells(_handler)
 
     return "".join(characters)
+
+def run_field(field, population_period=0.1, handler=lambda field: print(to_plaintext(field))):
+    previous_time = time.time()
+    while True:
+        handler(field)
+
+        field = field.populate()
+
+        current_time = time.time()
+        elapsed_time = current_time - previous_time
+        if elapsed_time < population_period:
+            time.sleep(population_period - elapsed_time)
+
+        previous_time = current_time
