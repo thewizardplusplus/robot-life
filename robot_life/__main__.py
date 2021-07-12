fe8f5beee94b2719df63c66db728a2100ea4a10c
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-07-12 23:00:00 +0300

    Add the main code

---
+++ robot_life/__main__.py
@@ -0,0 +1,16 @@
+import argparse
+
+from robot_life.console import run_random_field
+
+parser = argparse.ArgumentParser(
+    prog="python3 -m robot_life",
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+)
+parser.add_argument("-W", "--width", type=int, default=80, help="field width")
+parser.add_argument("-H", "--height", type=int, default=24, help="field height")
+parser.add_argument("-P", "--period",
+    type=float, default=0.1, help="population period")
+
+args = parser.parse_args()
+
+run_random_field(args.width, args.height, args.period)
