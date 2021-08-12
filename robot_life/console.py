78d720b5a6827a4a9365a2b899f3f519292c7347
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-08-12 23:00:00 +0300

    Complete the runner.basic_run_field() function

--- robot_life/console.py
+++ robot_life/console.py
@@ -20,15 +20,21 @@ def to_plaintext(field):
 def print_as_plaintext(field):
     print(to_plaintext(field))
 
-def run_field(field, population_period=0.1, handler=print_as_plaintext):
-    basic_run_field(field, handler, population_period)
+def run_field(
+    field,
+    population_period=0.1,
+    maximal_history_capacity=1_000_000,
+    handler=print_as_plaintext,
+):
+    basic_run_field(field, handler, population_period, maximal_history_capacity)
 
 # resolution of terminal VT100 by default
 def run_random_field(
     width=80,
     height=24,
     population_period=0.1,
+    maximal_history_capacity=1_000_000,
     handler=print_as_plaintext,
 ):
     field = Field(width, height, Field.random_generator)
-    run_field(field, population_period, handler)
+    run_field(field, population_period, maximal_history_capacity, handler)
--- robot_life/runner.py
+++ robot_life/runner.py
@@ -4,14 +4,22 @@ def basic_run_field(
     field,
     handler,
     population_period=0.1,
+    maximal_history_capacity=1_000_000,
     timer=time.time,
     sleeper=time.sleep,
 ):
+    field_history = [field]
     previous_time = timer()
     while True:
         handler(field)
 
         field = field.populate()
+        if field in field_history:
+            break
+
+        field_history.append(field)
+        if len(field_history) > maximal_history_capacity:
+            field_history.pop(0)
 
         current_time = timer()
         elapsed_time = current_time - previous_time
