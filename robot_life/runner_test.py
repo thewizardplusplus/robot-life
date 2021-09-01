8e427c58dbae2e33568a5593f862c25c6acb69cf
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-09-01 23:00:00 +0300

    Complete the tests of the runner.basic_run_field() function

--- robot_life/runner_test.py
+++ robot_life/runner_test.py
@@ -4,7 +4,7 @@ from robot_life.field import Field
 from robot_life.runner import basic_run_field
 
 class TestBasicRunField(unittest.TestCase):
-    def test_basic_run_field(self):
+    def test_basic(self):
         initial_field = Field.from_cell_rows([
             [False, False, False, False, False, False, False, False, False],
             [False, True,  False, False, False, False, False, False, False],
@@ -112,7 +112,7 @@ class TestBasicRunField(unittest.TestCase):
             wanted_field_histories,
         ])
 
-    def test_basic_run_field_with_stable_pattern(self):
+    def test_with_stable_pattern(self):
         initial_field = Field.from_cell_rows([
             [False, False, False, False],
             [False, True,  True,  False],
@@ -135,7 +135,7 @@ class TestBasicRunField(unittest.TestCase):
             ]),
         ]])
 
-    def test_basic_run_field_with_oscillator(self):
+    def test_with_oscillator(self):
         initial_field = Field.from_cell_rows([
             [False, False, False, False, False],
             [False, False, True,  False, False],
@@ -171,7 +171,7 @@ class TestBasicRunField(unittest.TestCase):
             wanted_field_histories,
         ])
 
-    def test_basic_run_field_with_low_history_capacity(self):
+    def test_with_low_history_capacity(self):
         initial_field = Field.from_cell_rows([
             [False, False, False, False, False],
             [False, False, True,  False, False],
@@ -211,4 +211,98 @@ class TestBasicRunField(unittest.TestCase):
             [wanted_field_histories[0]],
             [wanted_field_histories[1]],
             [wanted_field_histories[0]],
+        ])
+
+    def test_with_delay_check_and_elapsed_time_less_than_population_period(self):
+        initial_field = Field.from_cell_rows([
+            [False, False, False, False, False],
+            [False, False, True,  False, False],
+            [False, False, True,  False, False],
+            [False, False, True,  False, False],
+            [False, False, False, False, False],
+        ])
+
+        field_histories = []
+        def _handler(field_history):
+            field_histories.append(field_history.copy())
+
+        time = 1_000_000
+        def _timer():
+            nonlocal time
+            time += 0.025
+            return time
+
+        delays = []
+        def _sleeper(delay):
+            delays.append(delay)
+
+        basic_run_field(initial_field, _handler, timer=_timer, sleeper=_sleeper)
+
+        wanted_field_histories = [
+            Field.from_cell_rows([
+                [False, False, False, False, False],
+                [False, False, True,  False, False],
+                [False, False, True,  False, False],
+                [False, False, True,  False, False],
+                [False, False, False, False, False],
+            ]),
+            Field.from_cell_rows([
+                [False, False, False, False, False],
+                [False, False, False, False, False],
+                [False, True,  True,  True,  False],
+                [False, False, False, False, False],
+                [False, False, False, False, False],
+            ]),
+        ]
+        self.assertEqual(field_histories, [
+            wanted_field_histories[:1],
+            wanted_field_histories,
+        ])
+        self.assertEqual(len(delays), 1)
+        if len(delays) == 1:
+            self.assertAlmostEqual(delays[0], 0.075)
+
+    def test_with_delay_check_and_elapsed_time_greater_than_population_period(self):
+        initial_field = Field.from_cell_rows([
+            [False, False, False, False, False],
+            [False, False, True,  False, False],
+            [False, False, True,  False, False],
+            [False, False, True,  False, False],
+            [False, False, False, False, False],
+        ])
+
+        field_histories = []
+        def _handler(field_history):
+            field_histories.append(field_history.copy())
+
+        time = 1_000_000
+        def _timer():
+            nonlocal time
+            time += 0.25
+            return time
+
+        def _sleeper(delay):
+            raise RuntimeError("should not be called")
+
+        basic_run_field(initial_field, _handler, timer=_timer, sleeper=_sleeper)
+
+        wanted_field_histories = [
+            Field.from_cell_rows([
+                [False, False, False, False, False],
+                [False, False, True,  False, False],
+                [False, False, True,  False, False],
+                [False, False, True,  False, False],
+                [False, False, False, False, False],
+            ]),
+            Field.from_cell_rows([
+                [False, False, False, False, False],
+                [False, False, False, False, False],
+                [False, True,  True,  True,  False],
+                [False, False, False, False, False],
+                [False, False, False, False, False],
+            ]),
+        ]
+        self.assertEqual(field_histories, [
+            wanted_field_histories[:1],
+            wanted_field_histories,
         ])
